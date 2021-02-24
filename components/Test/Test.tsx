e0e3aac5948f4aa0316dc2102198a566b7cdc51b
Patryk Ordon <patryk.ordon@likims.com>
2021-02-24 19:11:26 +0100

    test cmp

---
+++ components/Test/Test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Test = ({}) => (
+  <>
+    <div className="bg-white dark:bg-gray-800 rounded-tl-xl sm:rounded-xl p-4 pb-6 sm:p-8 lg:p-4 lg:pb-6 xl:p-8 space-y-6 sm:space-y-8 lg:space-y-6 xl:space-y-8">
+      <p className="dark:text-white text-center">Hello World</p>
+    </div>
+  </>
+);
