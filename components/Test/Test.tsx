aa6986c506adbff7877dd7d2732a99965233983f
Patryk Ordon <patryk.ordon@likims.com>
2021-10-23 10:22:52 +0200

    remove tailwind & add styled components

--- components/Test/Test.tsx
+++ components/Test/Test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
+import * as Styled from "./Test.styled";
+
 export const Test = ({}) => (
-  <>
-    <div className="bg-white dark:bg-gray-800 rounded-xl sm:rounded-xl p-4 pb-6 sm:p-8 lg:p-4 lg:pb-6 xl:p-8 space-y-6 sm:space-y-8 lg:space-y-6 xl:space-y-8">
-      <p className="dark:text-white text-center">Hello World</p>
-    </div>
-  </>
+  <Styled.Wrapper>
+    <h1>Hello World 😀</h1>
+  </Styled.Wrapper>
 );
