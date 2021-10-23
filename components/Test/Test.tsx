b16de4bbb7215ab34fd965d4b552c95030cb4f3a
Patryk Ordon <patryk.ordon@likims.com>
2021-10-23 13:11:59 +0200

    last test of deployment

--- components/Test/Test.tsx
+++ components/Test/Test.tsx
@@ -4,7 +4,7 @@ import * as Styled from "./Test.styled";
 
 export const Test = ({}) => (
   <Styled.Wrapper>
-    <h1>Hello World 😀</h1>
-    <h1>😀 👊</h1>
+    <h1>Hello, my name is Patryk</h1>
+    <h1>😀👊</h1>
   </Styled.Wrapper>
 );
